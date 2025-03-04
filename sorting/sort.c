5034f18f2875d1bac2a6d636f42eac376033177f
serafi-nebot-uib <serafi.nebot1@estudiant.uib.cat>
2025-03-04 18:30:21 +0100

    fixed quicksort

--- sorting/sort.c
+++ sorting/sort.c
@@ -3,38 +3,13 @@
 #include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
+#include "colors.h"
+
+#define MAX 100
+#define MIN 0
 #define swap(a, b) { typeof(a) t = a; a = b; b = t; }
-
-void selection(int *arr, unsigned int len) {
-    for (unsigned int i = 0; i < len; i++) {
-        unsigned int p = i;
-        for (unsigned int j = i; j < len; j++)
-            if (arr[j] < arr[p]) p = j;
-        if (p != i) {
-            unsigned int t = arr[i];
-            arr[i] = arr[p];
-            arr[p] = t;
-        }
-    }
-}
-
-void quicksort_partition(int *arr, unsigned int l, unsigned int r) {
-    if ((int)(r - l) < 2) return;
-    unsigned int s = l,  p = r, t = 0;
-    for (unsigned int i = s; i < p; i++) {
-        if (arr[i] >= arr[p]) continue;
-        swap(arr[s], arr[i]);
-        s++;
-    }
-    swap(arr[s], arr[p]);
-    quicksort_partition(arr, l, s-1);
-    quicksort_partition(arr, s+1, r);
-}
-
-void quicksort(int *arr, unsigned int len) {
-    quicksort_partition(arr, 0, len-1);
-}
 
 void arr_print(int *arr, unsigned int len) {
     for (unsigned int i = 0; i < len; i++) printf("%d ", arr[i]);
@@ -49,28 +24,76 @@ double time_elapsed_ms(clock_t start, clock_t end) {
     return (double)(end - start) * 1000.0 / CLOCKS_PER_SEC;
 }
 
-void test(const char *name, void (f)(int *arr, unsigned int len), int *src, unsigned int len) {
-    int *arr = (int *) malloc(len);
+bool is_ordered(int *arr, unsigned int len) {
+    for (unsigned int i = 0; i < len-1; i++)
+        if (arr[i] > arr[i+1]) return false;
+    return true;
+}
+
+void test(const char *name, void (*f)(int *, unsigned int), int *src, unsigned int len) {
+    int *arr = (int *) malloc(len * sizeof(*src));
     if (arr == NULL) return;
     arr_cpy(src, arr, len);
 
     clock_t start = clock();
     f(arr, len);
     clock_t end = clock();
-    printf("%10s %8.3f ms: ", name, time_elapsed_ms(start, end));
-    arr_print(arr, len);
+
+    const char *state = is_ordered(arr, len) ? GRN "PASSED" RESET : RED "FAILED" RESET;
+    printf("%10s %10.3f ms %s\n", name, time_elapsed_ms(start, end), state);
 
     free(arr);
 }
 
+void selection(int *arr, unsigned int len) {
+    for (unsigned int i = 0; i < len; i++) {
+        unsigned int p = i;
+        for (unsigned int j = i; j < len; j++) if (arr[j] < arr[p]) p = j;
+        if (p != i) swap(arr[i], arr[p]);
+    }
+}
+
+void _quicksort(int *arr, unsigned int l, unsigned int r) {
+    if ((int)(r - l) < 1) return;
+    unsigned int s = l, p = r;
+    for (unsigned int i = s; i < p; i++) {
+        if (arr[i] >= arr[p]) continue;
+        swap(arr[s], arr[i]);
+        s++;
+    }
+    swap(arr[s], arr[p]);
+    _quicksort(arr, l, s-1);
+    _quicksort(arr, s+1, r);
+}
+
+void quicksort(int *arr, unsigned int len) {
+    _quicksort(arr, 0, len-1);
+}
+
+int compare(const void *a, const void *b) {
+    return *((int *)a) - *((int *)b);
+}
+
+void qsort_internal(int *arr, unsigned int len) {
+    qsort(arr, len, sizeof(*arr), compare);
+}
+
 int main(int argc, char **argv) {
-    int src[] = { 5, 7, 1, 3, 8, 9, 2, 4, 6 };
-    unsigned int n = sizeof(src) / sizeof(*src);
-    printf("src: ");
-    arr_print(src, sizeof(src) / sizeof(*src));
+    if (argc < 2) return -1;
+
+    unsigned int n = atoi(argv[1]);
+    int *src = (int *) malloc(n * sizeof(int));
+    if (src == NULL) return -1;
+
+    // srand(0);
+    srand(time(NULL));
+    for (unsigned int i = 0; i < n; i++) src[i] = rand() % (MAX - MIN + 1) + MIN;
 
     test("selection", selection, src, n);
     test("quicksort", quicksort, src, n);
+    test("qsort", qsort_internal, src, n);
+
+    free(src);
 
     return 0;
 }
