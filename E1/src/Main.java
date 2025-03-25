cb1f612f55c4cfbef8c43326e27015b14b0af1b6
serafi-nebot-uib <serafi.nebot1@estudiant.uib.cat>
2025-03-25 12:53:09 +0100

    finished E1

--- E1/src/Main.java
+++ E1/src/Main.java
@@ -1,47 +1,48 @@
-import com.sun.xml.internal.ws.addressing.WsaActionUtil;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Collections;
 import java.util.PriorityQueue;
+import java.util.Scanner;
+import java.util.Collections;
+
+// time complexity of PriorityQueue's methods:
+//   offer(): O(log n)
+//   peek(): O(1)
+//   poll(): O(1)
 
 public class Main {
     public static void main(String[] args) {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
-            String str;
-            while ((str = reader.readLine()) != null) {
-                if (str.equals("0")) break;
+        Scanner scanner = new Scanner(System.in);
 
-                int h = Integer.parseInt(str);
-                if ((str = reader.readLine()) == null) break;
-                String[] arr = str.trim().split("\\s+");
-                if (h != arr.length) break;
+        int h;
+        // iterate until hangar size, h, is equal to 0
+        // O(n log n) + O(n) + O(n log n) = O(n log n)
+        while ((h = scanner.nextInt()) > 0) {
+            // create a priority queue with a maximum capacity of h elements and reverse the order with
+            // Collections.reverseOrder() so that elements are ordered in descending order
+            PriorityQueue<Integer> heap = new PriorityQueue<>(h, Collections.reverseOrder());
+            // O(n) * O(log n) = O(n log n)
+            for (int i = 0; i < h; i++) // O(n)
+                heap.offer(scanner.nextInt()); // O(log n)
 
-                PriorityQueue<Integer> heap = new PriorityQueue<>(h, Collections.reverseOrder());
-                for (String s : arr) heap.offer(Integer.parseInt(s));
+            // read all spaceships and store them in an array in the order of arrival
+            int n = scanner.nextInt();
+            int[] spaceships = new int[n];
+            // O(n)
+            for (int i = 0; i < n; i++) spaceships[i] = scanner.nextInt();
 
-                if ((str = reader.readLine()) == null) break;
-                int n = Integer.parseInt(str);
-                if ((str = reader.readLine()) == null) break;
-                arr = str.trim().split("\\s+");
-                if (arr.length != n) break;
-
-                boolean fit = true;
-                for (int i = 0; i < arr.length && fit; i++) {
-                    if (!heap.isEmpty()) {
-                        int hangar = heap.poll();
-                        int nave = Integer.parseInt(arr[i]);
-                        int res = hangar - nave;
-                        if (res > 0) heap.offer(res);
-                        else if (res < 0) fit = false;
-                    } else {
-                        fit = false;
-                    }
-                }
-                System.out.println(fit ? "SI" : "NO");
+            boolean fit = true; // indicates whether all spaceships fit into the hangars following the assignment method
+            // iterate over all spaceships while `fit` is still true
+            // O(n) * (O(1) + O(log n)) = O(n) + O(n log n) = O(n log n)
+            for (int i = 0; i < n && fit; i++) { // O(n)
+                // when there are no hangars left or
+                //   the current spaceship cannot fit in the largest hangar (the first element in the queue)
+                //   the assignment method does not work for this case -> fit = false
+                if (heap.isEmpty() || heap.peek() < spaceships[i]) fit = false; // O(1)
+                // when the current spaceship fits into the largest hangar -> remove the first hangar and
+                //   add it back to the queue with the new size
+                else heap.offer(heap.poll() - spaceships[i]); // O(log n) + O(1) = O(log n)
             }
-        } catch (Exception ignored) {
-//            throw new RuntimeException(ignored);
+            System.out.println(fit ? "SI" : "NO");
         }
+
+        scanner.close();
     }
 }
