b8fd016cf65309489a6efe8eb18c403c46367342
serafi-nebot-uib <serafi.nebot1@estudiant.uib.cat>
2025-03-26 13:12:03 +0100

    fixed code requirements

--- E1/src/Main.java
+++ E1/src/Main.java
@@ -1,6 +1,26 @@
 import java.util.PriorityQueue;
 import java.util.Scanner;
-import java.util.Collections;
+
+class Hangar implements Comparable<Hangar> {
+    private int capacity;
+
+    public Hangar(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    @Override
+    public int compareTo(Hangar o) {
+        return o.capacity - this.capacity;
+    }
+}
 
 // time complexity of PriorityQueue's methods:
 //   offer(): O(log n)
@@ -11,34 +31,37 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int h;
         // iterate until hangar size, h, is equal to 0
-        // O(n log n) + O(n) + O(n log n) = O(n log n)
-        while ((h = scanner.nextInt()) > 0) {
+        // O(n log n) + O(n log n) = O(n log n)
+        for (int h = scanner.nextInt(); h != 0; h = scanner.nextInt()) {
             // create a priority queue with a maximum capacity of h elements and reverse the order with
             // Collections.reverseOrder() so that elements are ordered in descending order
-            PriorityQueue<Integer> heap = new PriorityQueue<>(h, Collections.reverseOrder());
+            PriorityQueue<Hangar> heap = new PriorityQueue<>(h);
             // O(n) * O(log n) = O(n log n)
             for (int i = 0; i < h; i++) // O(n)
-                heap.offer(scanner.nextInt()); // O(log n)
+                heap.offer(new Hangar(scanner.nextInt())); // O(log n)
 
-            // read all spaceships and store them in an array in the order of arrival
-            int n = scanner.nextInt();
-            int[] spaceships = new int[n];
-            // O(n)
-            for (int i = 0; i < n; i++) spaceships[i] = scanner.nextInt();
-
+            int n = scanner.nextInt(); // number of spaceships
             boolean fit = true; // indicates whether all spaceships fit into the hangars following the assignment method
             // iterate over all spaceships while `fit` is still true
             // O(n) * (O(1) + O(log n)) = O(n) + O(n log n) = O(n log n)
-            for (int i = 0; i < n && fit; i++) { // O(n)
-                // when there are no hangars left or
-                //   the current spaceship cannot fit in the largest hangar (the first element in the queue)
-                //   the assignment method does not work for this case -> fit = false
-                if (heap.isEmpty() || heap.peek() < spaceships[i]) fit = false; // O(1)
-                // when the current spaceship fits into the largest hangar -> remove the first hangar and
-                //   add it back to the queue with the new size
-                else heap.offer(heap.poll() - spaceships[i]); // O(log n) + O(1) = O(log n)
+            for (int i = 0; i < n; i++) { // O(n)
+                int spaceship = scanner.nextInt();
+                if (!fit) continue;
+                Hangar hangar = heap.peek(); // O(1)
+                // hangar = null when heap is empty -> no need to call heap.isEmpty()
+                if (hangar == null || hangar.getCapacity() < spaceship) { // O(1)
+                    // when there are no hangars left or
+                    //   the current spaceship cannot fit in the largest hangar (the first element in the queue)
+                    //   the assignment method does not work for this case -> fit = false
+                    fit = false;
+                } else { // O(1) + O(log n) = O(log n)
+                    // when the current spaceship fits into the largest hangar -> remove the first hangar and
+                    //   add it back to the queue with the new size
+                    heap.poll(); // O(1)
+                    hangar.setCapacity(hangar.getCapacity() - spaceship);
+                    if (hangar.getCapacity() > 0) heap.offer(hangar); // O(log n)
+                }
             }
             System.out.println(fit ? "SI" : "NO");
         }
